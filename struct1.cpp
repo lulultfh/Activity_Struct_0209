61d37af95b7d1ad86236aeb6cbfe46a1209a5413
lulultfh <punyalulu05@gmail.com>
2024-04-02 15:37:49 +0700

    membuat display output untuk input data yang sudah dibuat pada int main

--- struct1.cpp
+++ struct1.cpp
@@ -17,4 +17,10 @@ int main() {
     cin >> mhs.alamat;
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur; 
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.NIM;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
 }
