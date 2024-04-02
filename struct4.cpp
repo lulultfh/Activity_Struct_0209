6119a5467af0eb9ac30e1fcd0a438eedb24ebc1c
lulultfh <punyalulu05@gmail.com>
2024-04-02 16:42:25 +0700

    membuat perulangan untuk display output dengan for

--- struct4.cpp
+++ struct4.cpp
@@ -32,4 +32,15 @@ int Main () {
         cout << endl;
 
     }
+    for (int i=0; i<3; i ++) {
+        cout << endl;
+        cout << "Data Mahasiswa Ke- " << (i+1) << ":" << endl;
+        cout << "\n NIM = " << mhs[i].NIM;
+        cout << "\n Nama = " << mhs[i].nama;
+        cout << "\n Alamat = ";
+        cout << "\n Desa = " << mhs[i].alamat.desa;
+        cout << "\n Kota = " << mhs[i].alamat.kota;
+        cout << "\n Umur = " << mhs[i].umur;
+        cout << endl;
+    }
 }
