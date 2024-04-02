e6726e7f2b76486279c62dc42652a2c7b54e658d
lulultfh <punyalulu05@gmail.com>
2024-04-02 15:48:00 +0700

    membuat input data nim, nama, alamat, umur

--- struct2.cpp
+++ struct2.cpp
@@ -12,3 +12,18 @@ struct Mahasiswa {
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+}
