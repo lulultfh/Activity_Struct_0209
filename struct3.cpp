4638ce43a02dc0f98d1ae84125cf1c7601b9fad6
lulultfh <punyalulu05@gmail.com>
2024-04-02 16:06:19 +0700

    add input data (nomor mahasiswa, nama mahasiswa, alamat, umur) mahasiswa pada int main

--- struct3.cpp
+++ struct3.cpp
@@ -17,4 +17,15 @@ struct Mahasiswa {
 
 int Main () {
     Mahasiswa mhs;
+    cout << "Nomor Mahasiswa = ";
+    getline(cin, mhs.NIM);
+    cout << "Nama Mahasiswa = ";
+    getline(cin, mhs.nama);
+    cout << "Alamat Mahasiswa = " << endl;
+    cout << "\t Nama Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota = ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa = ";
+    cin >> mhs.umur;
 }
