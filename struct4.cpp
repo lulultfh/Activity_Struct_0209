5e2bc920ec402585b7118589d6958cfb3781acff
lulultfh <punyalulu05@gmail.com>
2024-04-02 16:36:51 +0700

    membuat perulangan untuk input data dengan for

--- struct4.cpp
+++ struct4.cpp
@@ -15,4 +15,21 @@ struct Mahasiswa {
 
 int Main () {
     Mahasiswa mhs[3];
+    for (int i = 0; i < 3; i++) {
+        cout << "Data Mahasiswa Ke- " << (i+1) << ":" << endl;
+        cout << "Nomor Mahasiswa = ";
+        getline(cin, mhs[i].NIM);
+        cout << "Nama Mahasiswa = ";
+        getline(cin, mhs[i].nama);
+        cout << "Alamat Mahasiswa = " << endl;
+        cout << "\t Nama Desa = ";
+        cin >> mhs[i].alamat.desa;
+        cout << "\t Nama Kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Umur Mahasiswa = ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+        cout << endl;
+
+    }
 }
